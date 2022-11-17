a7a98a1b03dae0e7676089facc526f376f3a9627
ZaidThahir <zaid@drpaw.life>
2022-11-17 16:05:52 +0530

    Zaid First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OtpPageComponent } from './pages/otp-page/otp-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:OtpPageComponent,path:'otp'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[
+  OtpPageComponent
+]
